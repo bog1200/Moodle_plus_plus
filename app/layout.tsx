437d0685ef146f975a35022e88a24010213c5829
Bogdan Stefanescu <26150024+bog1200@users.noreply.github.com>
2024-11-06 14:05:44 +0200

    Build: fix build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Aside from "./components/Aside";
 import localFont from "next/font/local";
 import "./globals.css";
 
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,35 +1,35 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-async function createEntries() {
-    const newAccount = await prisma.account.create({
-        data: {
-            ssoId: '5fe64736-a940-4180-946b-aaa37adf84a5',
-            firstName: 'Alexandru',
-            lastName: 'Soponaru',
-            email: 'alexandru.soponaru@gmail.com',
-            phone: '0728521521',
-            address: 'Str. Mihai Eminescu, nr. 1, Bucuresti',
-            gender: 'Male',
-            dob: new Date('2002-09-18')
-        },
-    });
-
-    const newStudent = await prisma.student.create({
-        data: {
-            accountId: newAccount.id
-        },
-    });
-
-    console.log('New account created:', newAccount);
-    console.log('New student created:', newStudent);
-}
-
-createEntries()
-    .catch(e => {
-        throw e;
-    })
-    .finally(async () => {
-        await prisma.$disconnect();
-    });
+// import { PrismaClient } from '@prisma/client';
+//
+// const prisma = new PrismaClient();
+//
+// async function createEntries() {
+//     const newAccount = await prisma.account.create({
+//         data: {
+//             ssoId: '5fe64736-a940-4180-946b-aaa37adf84a5',
+//             firstName: 'Alexandru',
+//             lastName: 'Soponaru',
+//             email: 'alexandru.soponaru@gmail.com',
+//             phone: '0728521521',
+//             address: 'Str. Mihai Eminescu, nr. 1, Bucuresti',
+//             gender: 'Male',
+//             dob: new Date('2002-09-18')
+//         },
+//     });
+//
+//     const newStudent = await prisma.student.create({
+//         data: {
+//             accountId: newAccount.id
+//         },
+//     });
+//
+//     console.log('New account created:', newAccount);
+//     console.log('New student created:', newStudent);
+// }
+//
+// createEntries()
+//     .catch(e => {
+//         throw e;
+//     })
+//     .finally(async () => {
+//         await prisma.$disconnect();
+//     });
