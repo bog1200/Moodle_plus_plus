4c14fa73da13aa78bdc13c0168678e6d6cdc66b3
Bogdan Stefanescu <26150024+bog1200@users.noreply.github.com>
2024-11-13 23:45:18 +0200

    DB Schema change + seed
    Created seed file + script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,35 +1,83 @@
-// import { PrismaClient } from '@prisma/client';
-//
-// const prisma = new PrismaClient();
-//
-// async function createEntries() {
-//     const newAccount = await prisma.account.create({
-//         data: {
-//             ssoId: '5fe64736-a940-4180-946b-aaa37adf84a5',
-//             firstName: 'Alexandru',
-//             lastName: 'Soponaru',
-//             email: 'alexandru.soponaru@gmail.com',
-//             phone: '0728521521',
-//             address: 'Str. Mihai Eminescu, nr. 1, Bucuresti',
-//             gender: 'Male',
-//             dob: new Date('2002-09-18')
-//         },
-//     });
-//
-//     const newStudent = await prisma.student.create({
-//         data: {
-//             accountId: newAccount.id
-//         },
-//     });
-//
-//     console.log('New account created:', newAccount);
-//     console.log('New student created:', newStudent);
-// }
-//
-// createEntries()
-//     .catch(e => {
-//         throw e;
-//     })
-//     .finally(async () => {
-//         await prisma.$disconnect();
-//     });
+ import { PrismaClient } from '@prisma/client';
+
+ const prisma = new PrismaClient();
+
+ async function createEntries() {
+
+    const account = await prisma.account.findFirst({
+        where: {
+            providerAccountId: '22db620a-0dc3-11ee-a70b-ca8b19b5aaf1'
+        }
+    })
+
+     const user = await prisma.user.findFirst({
+         where: {
+             accounts: {
+                 some: {
+                     providerAccountId: account!.providerAccountId
+                 }
+             }
+         }
+     });
+
+    const studentGroup = await prisma.studentGroup.create({
+        data: {
+            name: 'Group 1',
+        }
+    });
+
+   const student =  await prisma.student.create({
+        data: {
+            userId: account!.userId!,
+            groupId: studentGroup.id,
+        }
+    });
+
+    const teacher = await prisma.teacher.create({
+        data: {
+            userId: account!.userId!,
+        }
+    });
+
+    const subject = await prisma.subject.create({
+        data: {
+            name: 'Math',
+            teacherId: teacher.id
+        }
+    });
+
+     await prisma.subjectEnrollment.create({
+        data: {
+            studentId: student.id,
+            subjectId: subject.id
+        }
+    });
+
+    await prisma.file.create({
+        data: {
+            fileName: 'song1.mp3',
+            fileType: "application/mp3",
+            fileLink: "mpp/22db620a-0dc3-11ee-a70b-ca8b19b5aaf1/1731353978591-All i want for Christmas is You.mp3",
+            ownerId: user!.id
+    }
+    });
+
+    await prisma.file.create({
+        data: {
+            fileName: 'song2.mp3',
+            fileType: "application/mp3",
+            fileLink: "mpp/22db620a-0dc3-11ee-a70b-ca8b19b5aaf1/1731436857489-song.mp3",
+            ownerId: user!.id
+    }
+    });
+ }
+
+
+
+ createEntries()
+     .catch(e => {
+         throw e;
+     })
+     .finally(async () => {
+         await prisma.$disconnect();
+     });
