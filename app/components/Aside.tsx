882b831883d23f25f09c7c00585d2d83e795944f
Bogdan Stefanescu <26150024+bog1200@users.noreply.github.com>
2024-11-13 20:57:05 +0200

    Mobile UI v1

--- app/components/Aside.tsx
+++ app/components/Aside.tsx
@@ -15,43 +15,41 @@ export default async function Aside() {
 
 
     return (
-        <aside className="flex flex-col h-screen overflow-clip justify-around items-center border bg-gray-100 shadow-md">
+        <aside className="flex flex-row lg:flex-col h-full lg:h-screen overflow-clip justify-around items-center border bg-gray-100 shadow-md">
             <div className="flex">
-                <Image src="/favicon.ico" alt="Logo" width={150} height={150} className="rounded-full"/>
+                <Image src="/favicon.ico" alt="Logo" width={75} height={75} className="rounded-full"/>
 
             </div>
-            <div className="flex flex-col items-center  overflow-hidden">
-                <ul>
+            <div className="flex flex-row lg:flex-col items-center z-10">
+                <ul className={"flex lg:block"}>
                     <li className="">
-                        <Link href="/dashboard"  className="mr-10">
+                        <Link href="/dashboard"  className="lg:mr-10">
                         <SvgIcon heading={"Dashboard"} svg={ <AcademicCap color={"rgb(134, 8, 140)"}/>}/>
                     </Link>
                     </li>
                     <li>
-                        <Link href="/dashboard/courses" className="mr-10">
+                        <Link href="/dashboard/courses" className="lg:mr-10">
                             {/*<Image src={calendarCheck} alt="calendar check" width={30} height={30}/>*/}
                            <SvgIcon heading={"Courses"} svg={<BookOpen color={"rgb(22, 87, 171)"} /> }/>
                         </Link>
                     </li>
                     <li>
-                        <Link href="/dashboard/schedule" className="mr-10">
+                        <Link href="/dashboard/schedule" className="lg:mr-10">
                             {/*<Image src={calendarEventFill} alt="calendar event" width={30} height={30}/>*/}
                            <SvgIcon  heading={"Schedule"} svg={ <Calendar color={"#e8ca20"}/>} />
                         </Link>
                     </li>
                     <li>
-                    <Link href="/dashboard/profile" className="mr-10">
+                    <Link href="/dashboard/profile" className="lg:mr-10">
                         {/*<Image src={personFill} alt="profile" width={30} height={30}/>*/}
                       <SvgIcon heading={"Profile"} svg={ <User  color={"#3dc461"}/> }/>
                     </Link>
                     </li>
                     <li>
-                        <li>
-                            <Link href="/dashboard/file_manager" className="mr-10">
-                                {/*<Image src={calendarCheck} alt="calendar check" width={30} height={30}/>*/}
-                                <SvgIcon heading={"File Manager (Admin)"} svg={<BookOpen color={"#faa"}/>}/>
-                            </Link>
-                        </li>
+                        <Link href="/dashboard/file_manager" className="lg:mr-10">
+                            {/*<Image src={calendarCheck} alt="calendar check" width={30} height={30}/>*/}
+                            <SvgIcon heading={"File Manager (Admin)"} svg={<BookOpen color={"#faa"}/>}/>
+                        </Link>
                     </li>
                 </ul>
 
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,12 +1,13 @@
 import Aside from "@/app/components/Aside";
+import React from "react";
 
 export default  async function DashboardLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className={"w-full flex"}>
-            <div className={"sticky w-[10%]"}>
+        <div className={"lg:w-full flex min-h-screen"}>
+            <div className={"fixed bottom-0 w-full h-[10%] lg:sticky lg:top-0 lg:left-0  lg:w-[20%] lg:h-screen z-10"}>
                 <Aside />
             </div>
-            <div className={"w-[90%] p-2"}>
+            <div className={"w-full lg:w-[80%] h-[80%] lg:h-full p-2 mb-[10%] pb-4  lg:mb-0   "}>
                 {children}
             </div>
         </div>
