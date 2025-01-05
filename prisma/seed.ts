8c817e2fc0a3d8dda4a9f33d49f3668c1cfdb6e2
Bogdan Stefanescu <26150024+bog1200@users.noreply.github.com>
2025-01-05 22:10:52 +0200

    seed file - added demo assignments + submission

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,14 +1,20 @@
- import { PrismaClient } from '@prisma/client';
+ import {$Enums, PrismaClient} from '@prisma/client';
+ import AssignmentType = $Enums.AssignmentType;
 
  const prisma = new PrismaClient();
 
  async function createEntries() {
+
 
     const account = await prisma.account.findFirst({
         where: {
             providerAccountId: '22db620a-0dc3-11ee-a70b-ca8b19b5aaf1'
         }
     })
+
+    if (!account) {
+        throw new Error('Account not found');
+    }
 
      const user = await prisma.user.findFirst({
          where: {
@@ -70,6 +76,35 @@
             ownerId: user!.id
     }
     });
+
+    const assignment1= await prisma.assignment.create({
+        data: {
+            subjectId: subject.id,
+            title: 'Assignment 1',
+            description: 'Assignment 1 description',
+            dueDate: new Date(),
+            assignmentType: AssignmentType.HOMEWORK,
+        }
+    });
+
+    await prisma.assignmentSubmission.create({
+        data: {
+            assignmentId: assignment1.id,
+            studentId: student.id,
+            submissionDate: new Date(),
+        }
+    });
+
+    await prisma.assignment.create({
+        data: {
+            subjectId: subject.id,
+            title: 'Assignment 2',
+            description: 'Assignment 2 description',
+            dueDate: new Date(1738368000),
+            assignmentType: AssignmentType.HOMEWORK,
+        }
+    });
+
  }
 
 
